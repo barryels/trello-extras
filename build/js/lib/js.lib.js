86ec5940ed081a8b0fe9089e55f91d0ab55d4759
Barry Els <barryels@gmail.com>
2016-06-22 08:06:53 +0200

    switching from dir: 'bin' to dir: 'dist', so as to follow convention for front-end JS projects

--- build/js/lib/js.lib.js
+++ build/js/lib/js.lib.js
@@ -1,11 +1,12 @@
+var packageJSON = require('./../../../package.json');
 var fs = require('fs');
 var mkdirp = require('mkdirp');
-var libDirectory = 'bin/js/lib';
+var libDirectory = packageJSON.distDirectory + '/js/lib';
 
 mkdirp(libDirectory, function (err) {
 	if (err) {
 		console.error(err);
 	} else {
-		fs.createReadStream('./node_modules/jquery/dist/jquery.min.js').pipe(fs.createWriteStream('./bin/js/lib/jquery.min.js'));
+		fs.createReadStream('./node_modules/jquery/dist/jquery.min.js').pipe(fs.createWriteStream(libDirectory + '/jquery.min.js'));
 	}
 });
--- build/manifest.json/manifest.json.js
+++ build/manifest.json/manifest.json.js
@@ -9,7 +9,7 @@ manifestJSONTemplate.version = packageJSON.version;
 manifestJSONTemplate.description = packageJSON.description;
 manifestJSONTemplate.author = packageJSON.author;
 
-fs.writeFile('bin/manifest.json', JSON.stringify(manifestJSONTemplate, null, '	'), function (err) {
+fs.writeFile(packageJSON.distDirectory + '/manifest.json', JSON.stringify(manifestJSONTemplate, null, '	'), function (err) {
 	if (err) {
 		return console.log(err);
 	}
